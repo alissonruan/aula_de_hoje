e2a91a12967aba248f5b8821284e47f57e201e89
--local <--local>
2025-03-24 20:39:30 -0300

    mais condicoes

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php
-echo "hello world";
+$valor=-2;
+$valor1=-2;
+$variable;
+
+if (!($valor > 0) || $valor1 > 0){
+//verdadeiro
+
+echo "verdadeiro";
+}else{
+//falso
+
+echo "falso";
+}
+
+
+switch ($variable){
+    case 'A';
+    #code..
+    break;
+
+    case 'B';
+    #code..
+    break;
+
+    case 'C';
+    #code..
+    break;
+
+    default;
+}
+
 ?>
