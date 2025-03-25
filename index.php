13e0646ff9f517bf2cd4c2e97dd75a2e6d7c6651
--local <--local>
2025-03-24 21:46:30 -0300

    Condições while e for

--- index.php
+++ index.php
@@ -1,33 +1,44 @@
 <?php
-$valor=-2;
+$valor=30;
 $valor1=-2;
 $variable;
 
 if (!($valor > 0) || $valor1 > 0){
-//verdadeiro
-
-echo "verdadeiro";
+    //verdadeiro
+    
+    echo "verdadeiro";
 }else{
-//falso
-
-echo "falso";
+    //falso
+    
+    echo "falso";
 }
 
 
-switch ($variable){
+/*switch ($variable){
     case 'A';
     #code..
     break;
-
+    
     case 'B';
     #code..
     break;
-
+    
     case 'C';
     #code..
     break;
-
+    
     default;
-}
-
+    }*/
+    
+    while ($valor>10){
+        $valor -= 2;
+        echo "<br>Valor: ".$valor;
+    }   
+    
+    $i = 0;
+    
+    for ($i = 0; $i < 10; $i+=2){
+        echo "<br>valor de i: ".$i;
+    }
+    
 ?>
